afdf90b985c5b4448a6cdc3c60c8c01f3e9e75fa
Neeraj Kumar <neerajkumar@MacBook-Pro.local>
2023-08-26 15:46:01 +0530

    Longest Substring with non repeating characters solution variation

--- MandatoryProblems/3_Longest_Substring_Without_Repeating_Characters.cs
+++ MandatoryProblems/3_Longest_Substring_Without_Repeating_Characters.cs
@@ -15,3 +15,23 @@ public class Solution {
         return maxLength;
     }
 }
+
+// with Hashset Variation
+
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        HashSet<char> set = new HashSet<char>();
+        int left = 0, right = 0, maxLength = 0;
+        while(right<s.Length){
+            if(!set.Contains(s[right])){
+                set.Add(s[right]);
+                right++;
+                maxLength = Math.Max(maxLength, set.Count);
+            }else{
+                set.Remove(s[left]);
+                left++;
+            }
+        }
+        return maxLength;
+    }
+}
